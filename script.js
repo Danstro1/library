4a0d51341d5b353487e41833841a17432de1aa75
Timur <galactictimur@gmail.com>
2024-02-01 13:32:29 +0300

    Refractor to use classes instead plain constructors

--- script.js
+++ script.js
@@ -1,73 +1,92 @@
-const myLibrary = [];
-let count = 0;
 
-function Book(title, author, pages, read){
-    this.title = title;
-    this.author = author;
-    this.pages = pages;
-    this.read = read;
+class Book {
+    constructor(title, author, pages, read){
+        this.title = title;
+        this.author = author;
+        this.pages = pages;
+        this.read = read;
+    }
+
+    get bookTitle() {
+        return this.title;
+    }
+
+    get bookAuthor() {
+        return this.author;
+    }
+
+    get bookPages() {
+        return this.pages;
+    }
+
+    get bookRead() {
+        return this.read;
+    }
 }
 
-function addBookToLibrary(){
-    myLibrary.push(new Book(title.value, author.value, pages.value, read.value));
-
-    const cards = document.querySelector('.cards');
-    const newDiv = document.createElement('div');
-    const cardTitle = document.createElement('div');
-    const cardAuthor = document.createElement('div');
-    const cardPages = document.createElement('div');
-    const cardRead = document.createElement('button');
-    const cardRem = document.createElement('button');
-
-    cardTitle.textContent = myLibrary[count].title;
-    cardAuthor.textContent = myLibrary[count].author;
-    cardPages.textContent = myLibrary[count].pages;
-    cardRem.textContent = 'Remove'
-
-    cardTitle.classList.add('row-book');
-    cardAuthor.classList.add('row-book');
-    cardPages.classList.add('row-book');
-    cardRem.classList.add('but-rem');
-
-    newDiv.classList.add('card');
-
-
-    cards.appendChild(newDiv);
-    newDiv.appendChild(cardTitle);
-    newDiv.appendChild(cardAuthor);
-    newDiv.appendChild(cardPages);
-    newDiv.appendChild(cardRead);
-    if(read.checked){
-        cardRead.textContent = 'Read';
-        cardRead.classList.add('but-yes');
-    }else{
-        cardRead.textContent = 'Not Read';
-        cardRead.classList.add('but-no');  
+class Library {
+    constructor() {
+        this.myLibrary = [];
+        this.count = 0;
     }
-    newDiv.appendChild(cardRem);
 
-    title.value = null;
-    author.value = null;
-    pages.value = null;
+    addBook(title, author, pages, read){
+        this.myLibrary.push(new Book(title.value, author.value, pages.value, read.checked));
+        this.count++;
+    }
 
-    cardRead.addEventListener('click', () =>{
-        if(cardRead.classList.contains('but-yes')){
-          cardRead.classList.remove('but-yes');
-          cardRead.classList.add('but-no');
-          cardRead.textContent = 'Not Read';
+    displayBook(){
+        const cards = document.querySelector('.cards');
+        const newDiv = document.createElement('div');
+        const cardTitle = document.createElement('div');
+        const cardAuthor = document.createElement('div');
+        const cardPages = document.createElement('div');
+        const cardRead = document.createElement('button');
+        const cardRem = document.createElement('button');
+
+        cardTitle.textContent = this.myLibrary[this.count - 1].bookTitle;
+        cardAuthor.textContent = this.myLibrary[this.count - 1].bookAuthor;
+        cardPages.textContent = this.myLibrary[this.count - 1].bookPages;
+        cardRem.textContent = 'Remove'
+    
+        cardTitle.classList.add('row-book');
+        cardAuthor.classList.add('row-book');
+        cardPages.classList.add('row-book');
+        cardRem.classList.add('but-rem');
+        newDiv.classList.add('card');
+    
+    
+        cards.appendChild(newDiv);
+        newDiv.appendChild(cardTitle);
+        newDiv.appendChild(cardAuthor);
+        newDiv.appendChild(cardPages);
+        newDiv.appendChild(cardRead);
+        if(read.checked){
+            cardRead.textContent = 'Read';
+            cardRead.classList.add('but-yes');
+        }else{
+            cardRead.textContent = 'Not Read';
+            cardRead.classList.add('but-no');  
         }
-        else{
-          cardRead.classList.remove('but-no');
-          cardRead.classList.add('but-yes');
-          cardRead.textContent = 'Read';
-        }
-   });
-
-   cardRem.addEventListener('click', () =>{
-        cards.removeChild(newDiv);
-   })
-
-    count++;
+        newDiv.appendChild(cardRem);
+    
+        cardRead.addEventListener('click', () =>{
+            if(cardRead.classList.contains('but-yes')){
+              cardRead.classList.remove('but-yes');
+              cardRead.classList.add('but-no');
+              cardRead.textContent = 'Not Read';
+            }
+            else{
+              cardRead.classList.remove('but-no');
+              cardRead.classList.add('but-yes');
+              cardRead.textContent = 'Read';
+            }
+       });
+    
+       cardRem.addEventListener('click', () =>{
+            cards.removeChild(newDiv);
+       })
+    }
 }
 
 const submit = document.querySelector('.submit-button');
@@ -79,13 +98,19 @@ const pages = document.querySelector('#pages');
 const read = document.querySelector('#read');
 const body = document.querySelector('body');
 
+const lib = new Library();
 
 dlgButton.addEventListener('click', () => {dialog.showModal();});
 
 submit.addEventListener('click', e => {
     if(title.value !== "" && author.value !== "" && pages.value !== ""){
         e.preventDefault();
-        addBookToLibrary();
+        lib.addBook(title, author, pages, read);
+        lib.displayBook();
+        title.value = "";
+        author.value = "";
+        pages.value = "";
+        read.checked = false;
         dialog.close();
     }
 });
